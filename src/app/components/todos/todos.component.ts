69634cc4f2b20a57d89c2737effd85bf13d05cde
tunka8090 <tunka8090@yandex.ru>
2024-11-20 15:58:19 +0300

    [upd]: Реализация списка задачь

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
 export class TodosComponent {
-  todos: Todo[] = [
-    {
-      content: 'First todo',
-      completed: false,
-    },
-    {
-      content: 'Seccond todo',
-      completed: true,
-    },
-  ];
+  todos: Todo[] = [];
+
+  inputTodo: string = '';
+
+  toggleDone(id: number): void {
+    this.todos.forEach((value, index) => {
+      if (index === id) {
+        value.completed = !value.completed;
+      }
+    });
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter((value, index) => index !== id);
+  }
+
+  addTodo(): void {
+    if (this.inputTodo !== '') {
+      this.todos.push({
+        content: this.inputTodo,
+        completed: false,
+      });
+    } else {
+      alert('Введите задачу');
+    }
+
+    this.inputTodo = '';
+  }
 }
