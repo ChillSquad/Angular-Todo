8b65c2cccaa73821ffecaf940415574906dd18f8
tunka8090 <tunka8090@yandex.ru>
2024-11-20 12:39:23 +0300

    [ref]: Изменение исходного компонента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TodosComponent } from './components/todos/todos.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-todo';
